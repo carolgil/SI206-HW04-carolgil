0f036cd700046815b395deaf8df0de3bf0d01890
Emma Shpiz <eshpiz@umich.edu>
2018-01-30 14:59:21 -0500

    possible answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,4 @@
 response = input("What is your question?")
+import random
+possible_answers = ["It is certain", "It is decidedly", "Without a doubt", "Yes definitiely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"]
+pick(random.choice(possible_answers))
