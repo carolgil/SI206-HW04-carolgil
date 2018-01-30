fbf0d06b008bac4468e38d29e31c4206bb0613ed
Caroline Gilhool <carolgil@umich.edu>
2018-01-30 15:42:54 -0500

    add

--- magic_eight.py
+++ magic_eight.py
@@ -5,7 +5,8 @@ while response != "quit" :
     if not response.endswith("?"):
         print("I'm sorry, I can only answer questions")
     else:
-        possible_answers = ['Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful', "It is certain", "It is decidedly", "Without a doubt", "Yes definitiely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes"]
+        possible_answers = ['Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful', "It is certain", "It is decidedly", "Without a doubt", "Yes definitiely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes",
+        "Signs point to yes"]
         print(random.choice(possible_answers))
 
     response = input("What is your question?")
