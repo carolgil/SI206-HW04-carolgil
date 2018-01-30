6fddce2e5e88096384e66d60d4732a70e5292975
Sydney Lash <sydneylash@0587394694.wireless.umich.net>
2018-01-30 15:17:26 -0500

    add more_possible_answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,3 @@
 response = input("What is your question?")
-possible_answers = ['Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+more_possible_answers = ['Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
 print(random.choice(possible_answers))
