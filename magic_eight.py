060d8aadab4aac102f50c4601bdc0aac376e9875
Caroline Gilhool <carolgil@umich.edu>
2018-01-30 15:24:53 -0500

    question

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,5 @@
 response = input("What is your question?")
+while response != "quit" :
+    response = input("What is your question?")
+    if response[:-1] is not "?" :
+        print("I'm sorry, I can only answer questions")
